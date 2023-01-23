dc18888647a6cefd36ade4d25f05ea3ccdc4227c
AM <almehady@gmail.com>
2023-01-23 13:01:58 +0600

    refac: mysql server details added

--- znz/settings.py
+++ znz/settings.py
@@ -96,25 +96,25 @@ WSGI_APPLICATION = 'znz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-
 # DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'znz_db',
-#         'USER': 'root',
-#         'PASSWORD': 'TechTrioz@01',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'znz_db',
+        'USER': 'znz_user',
+        'PASSWORD': 'znzitALbsM2@23s',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
