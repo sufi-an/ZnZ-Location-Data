1e98378cf43026bf7ee00b0a3d851e009eb1ed9f
AM <almehady@gmail.com>
2023-01-25 15:46:58 +0600

    fix: admin list updated

--- tm_backend/admin.py
+++ tm_backend/admin.py
@@ -4,14 +4,21 @@ from tm_backend.models import Category, District, Division, Location, LocationPi
 
 #from .models import TruckTypeMaster, Truck, Factory, ProductMaster, Requisition, Trips, Item, req_item
 
+class UnionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'Thana_id', 'district_id', 'division_id')
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'division_id', 'district_id', 'thana_id', 'union_id', 'lat', 'long')
+
 
 # from .models import Customer
 admin.site.register(Category)
 admin.site.register(Division)
 admin.site.register(District)
 admin.site.register(Thana)
-admin.site.register(Union)
+admin.site.register(Union, UnionAdmin)
 admin.site.register(LocationPicture)
-admin.site.register(Location)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(route)
 
